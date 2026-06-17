f8b9f770490964ee1dd9baaf991365a619015cb1
Alper.ates <alper.ates@liderkozmetik.com>
2026-06-17 10:20:08 +0300

    fix: add missing using Npgsql; import in OrderFeatures.cs

--- backend/src/TrackTrace.Application/Features/Orders/OrderFeatures.cs
+++ backend/src/TrackTrace.Application/Features/Orders/OrderFeatures.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Dapper;
 using FluentValidation;
 using MediatR;
+using Npgsql;
 using TrackTrace.Application.Common;
 using TrackTrace.Application.Common.Interfaces;
 using TrackTrace.Domain.Enums;
